46b2626eba0d80216890adfb85b7833fd4da3faf
pallavimitra <pallavi.mitra1992@gmail.com>
2018-08-02 14:50:38 +0800

    Add files via upload

--- submit.m
+++ submit.m
@@ -107,47 +107,51 @@ end
 % ================== CONFIGURABLES FOR EACH HOMEWORK ==================
 
 function id = homework_id() 
-  id = '3';
+  id = '4';
 end
 
 function [partNames] = validParts()
-  partNames = { 'Vectorized Logistic Regression ', ...
-                'One-vs-all classifier training', ...
-                'One-vs-all classifier prediction', ...
-                'Neural network prediction function' ...
+  partNames = { 'Feedforward and Cost Function', ...
+                'Regularized Cost Function', ...
+                'Sigmoid Gradient', ...
+                'Neural Network Gradient (Backpropagation)' ...
+                'Regularized Gradient' ...
                 };
 end
 
 function srcs = sources()
   % Separated by part
-  srcs = { { 'lrCostFunction.m' }, ...
-           { 'oneVsAll.m' }, ...
-           { 'predictOneVsAll.m' }, ...
-           { 'predict.m' } };
+  srcs = { { 'nnCostFunction.m' }, ...
+           { 'nnCostFunction.m' }, ...
+           { 'sigmoidGradient.m' }, ...
+           { 'nnCostFunction.m' }, ...
+           { 'nnCostFunction.m' } };
 end
 
-function out = output(partId, auxdata)
+function out = output(partId, auxstring)
   % Random Test Cases
-  X = [ones(20,1) (exp(1) * sin(1:1:20))' (exp(0.5) * cos(1:1:20))'];
-  y = sin(X(:,1) + X(:,2)) > 0;
-  Xm = [ -1 -1 ; -1 -2 ; -2 -1 ; -2 -2 ; ...
-          1 1 ;  1 2 ;  2 1 ; 2 2 ; ...
-         -1 1 ;  -1 2 ;  -2 1 ; -2 2 ; ...
-          1 -1 ; 1 -2 ;  -2 -1 ; -2 -2 ];
-  ym = [ 1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4 ]';
+  X = reshape(3 * sin(1:1:30), 3, 10);
+  Xm = reshape(sin(1:32), 16, 2) / 5;
+  ym = 1 + mod(1:16,4)';
   t1 = sin(reshape(1:2:24, 4, 3));
   t2 = cos(reshape(1:2:40, 4, 5));
-
+  t  = [t1(:) ; t2(:)];
   if partId == 1
-    [J, grad] = lrCostFunction([0.25 0.5 -0.5]', X, y, 0.1);
+    [J] = nnCostFunction(t, 2, 4, 4, Xm, ym, 0);
+    out = sprintf('%0.5f ', J);
+  elseif partId == 2
+    [J] = nnCostFunction(t, 2, 4, 4, Xm, ym, 1.5);
+    out = sprintf('%0.5f ', J);
+  elseif partId == 3
+    out = sprintf('%0.5f ', sigmoidGradient(X));
+  elseif partId == 4
+    [J, grad] = nnCostFunction(t, 2, 4, 4, Xm, ym, 0);
     out = sprintf('%0.5f ', J);
     out = [out sprintf('%0.5f ', grad)];
-  elseif partId == 2
-    out = sprintf('%0.5f ', oneVsAll(Xm, ym, 4, 0.1));
-  elseif partId == 3
-    out = sprintf('%0.5f ', predictOneVsAll(t1, Xm));
-  elseif partId == 4
-    out = sprintf('%0.5f ', predict(t1, t2, Xm));
+  elseif partId == 5
+    [J, grad] = nnCostFunction(t, 2, 4, 4, Xm, ym, 1.5);
+    out = sprintf('%0.5f ', J);
+    out = [out sprintf('%0.5f ', grad)];
   end 
 end
 
